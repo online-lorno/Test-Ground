57b10d0c90d2f3371d855dce4f8bb36f52a6438a
brucdarc <darkdark@DESKTOP-LNM8S60.localdomain>
2020-11-19 21:24:56 -0700

    added some basic documentation

--- contracts/Citizenship.sol
+++ contracts/Citizenship.sol
@@ -4,6 +4,10 @@ pragma solidity =0.7.4;
 import "https://raw.githubusercontent.com/OpenZeppelin/openzeppelin-contracts/master/contracts/token/ERC721/ERC721.sol";
 import "https://raw.githubusercontent.com/OpenZeppelin/openzeppelin-contracts/master/contracts/access/Ownable.sol";
 
+/*
+mock up contract that lets people buy a roman citizenship as an erc721 token
+*/
+
 contract Citizenship is ERC721, Ownable {
 
     uint256 public citizenCount;
@@ -11,6 +15,11 @@ contract Citizenship is ERC721, Ownable {
     constructor () ERC721("RomanCitizenship", "RCIT") public {
         citizenCount = 0;
     }
+
+    /*
+    Function that lets people pay .5 eth to buy a citizenship token,
+    which is a nft erc721 token that makes them a member of the roman empire
+    */
 
     function buyCitizenship() public payable {
         require(msg.value == .5 ether, "Sent ether does not match citizenship price");
--- contracts/Rome.sol
+++ contracts/Rome.sol
@@ -5,15 +5,18 @@ import "https://raw.githubusercontent.com/OpenZeppelin/openzeppelin-solidity/mas
 //import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/math/SafeMath.sol";
 
 //using SafeMath for uint256;
-
-
+/*
+testing rome contract to figure out how to mint supporter erc721 tokens based on capital invested
+*/
 contract Rome is ERC20 {
     using SafeMath for uint256;
 
     constructor() ERC20("Rome", "ROME") public {
 
     }
-
+    /*
+    Takes eth and mints rome. 10 rome per eth. Testing function
+    */
 
     function buyRome(uint256 amount) public payable{
 
