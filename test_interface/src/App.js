a1a2bc7287bd37c8955b23373a4d96eedc3fb5c8
brucdarc <darkdark@DESKTOP-LNM8S60.localdomain>
2020-11-28 22:21:45 -0700

    added shroom stuff on front end for baseline

--- test_interface/src/App.js
+++ test_interface/src/App.js
@@ -8,54 +8,149 @@ import Container from 'react-bootstrap/Container'
 import Col from 'react-bootstrap/Col'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Card, Form} from "semantic-ui-react";
-import {React, Text} from "react";
+import {Component, React, Text} from "react";
 import Dashboard from "./components/Dashboard";
+import Mushroom from "./Mushroom";
+import Rome from "./Rome";
+
+
+//here and not in style sheet because css doesnt like text-shadow 33cd0c 35FFEC
+const radiantWhite = {color: 'white', margin: 0, 'text-shadow': '0px 0px 2px #35FFEC'}
+
+
+class App extends Component{
 
 
 
+    constructor(props){
+        super(props);
+
+        this.state={
+            waitingOnContract: false,
+            TotalMush : 0,
+            TotalUsdc: 0,
+            EstimatedUsdcValue: 0,
+            UserMush: 0,
+            UserUsdc: 0
+        };
+
+        this.Update = this.Update.bind(this);
+        this.UpdateWaitingOnContract = this.UpdateWaitingOnContract.bind(this);
+        this.updateContractStats = this.updateContractStats.bind(this);
+        //go fetch data from chain
+        this.updateContractStats();
+    }
+    /*
+    moved this here because we need to call it when invest or liquidate calls the update function, and having
+    something that calls setstate in a render method causes infinite recursion slowly
+    */
+
+    async updateContractStats() {
+
+        var accounts;
+
+        try {
+            await web3.eth.getAccounts().then(function (acc) {
+                accounts = acc
+            })
+
+            let total_mush = await Mushroom.methods.totalSupply().call();
+            let user_mush = await Mushroom.methods.balanceOf(accounts[0]).call();
+
+            let est_value = await Mushroom.methods.viewEstValue().call();
+
+            let total_usdc = await Rome.methods.balanceOf(Mushroom.options.address).call();
+            let user_usdc = await Rome.methods.balanceOf(accounts[0]).call();
+
+            total_mush = web3.utils.fromWei(total_mush, 'ether');
+            user_mush = web3.utils.fromWei(user_mush, 'ether');
+            est_value = web3.utils.fromWei(est_value, 'ether');
+            total_usdc = web3.utils.fromWei(total_usdc, 'ether');
+            user_usdc = web3.utils.fromWei(user_usdc, 'ether');
 
 
+            this.setState({
+                TotalMush: total_mush,
+                UserMush: user_mush,
+                TotalUsdc: total_usdc,
+                UserUsdc: user_usdc,
+                EstimatedUsdcValue: est_value
+            });
 
-function App() {
-  return (
-    <div className="App" style={{
-        backgroundImage: "url(" + "https://images8.alphacoders.com/600/600182.jpg" + ")",
-        backgroundPosition: 'center',
-        backgroundSize: 'cover',
-        backgroundRepeat: 'no-repeat'
-    }}>
-      <header className="App-header" style={{
-          backgroundColor: 'transparent'
-      }}>
-          <h1 style={{ color: 'white', margin: 0, 'text-shadow': '0px 0px 2px #35FFEC' }}>
-              Welcome to Mushroom Capital
-          </h1>
-          <br/><br/><br/><br/><br/>
-          <Dashboard/>
-      </header>
-        <Container>
 
-            <Row>
-                <Col sm>
-                    <Invest/>
-                </Col>
-                <Col sm>
-                    <Liquidate/>
-                </Col>
-            </Row>
-        </Container>
-        <header className="App-header" style={{
-            backgroundColor: 'transparent'
-        }}>
-            <h3 style={{ color: 'EBEBEB', margin: 0, 'text-shadow': '0px 0px 2px #35FFEC'   }}>
-                All initial investments are subject to up to a 1 month holding period to account for slippage.
-            </h3>
-            <h3 style={{ color: 'EBEBEB', margin: 0, 'text-shadow': '0px 0px 2px #35FFEC'   }}>
-                Prices may fluctuate, and Mushroom Capital is not responsible for temporary or permanent loss of funds.
-            </h3>
-        </header>
-    </div>
-  );
+        }
+        catch (err) {
+            console.log("ERROR IN SENDING TO CHAIN " + err);
+            this.setState({
+            });
+        }
+    }
+
+    Update() {
+        this.updateContractStats();
+        this.forceUpdate();
+        console.log("I got called!");
+    }
+
+    UpdateWaitingOnContract(bool) {
+        this.setState({
+            waitingOnContract: bool
+        });
+    }
+
+    render() {
+        return (
+            //set mushroom forest background, make it fill screen
+            <div className="App" style={{
+                backgroundImage: "url(" + "https://images8.alphacoders.com/600/600182.jpg" + ")",
+                backgroundPosition: 'center',
+                backgroundSize: 'cover',
+                backgroundRepeat: 'no-repeat'
+            }}>
+                <header className="App-header" style={{
+                    backgroundColor: 'transparent'
+                }}>
+                    <br/><br/><br/><br/>
+                    <h1 style={radiantWhite}> {/* neat white text with blue fading outline */}
+                        Welcome to Mushroom Capital
+                    </h1>
+                    <br/><br/><br/><br/><br/> {/*Tons of new lines*/}
+                    <Dashboard TotalMush={this.state.TotalMush} UserMush={this.state.UserMush}
+                               TotalUsdc={this.state.TotalUsdc} UserUsdc={this.state.UserUsdc}
+                               EstimatedUsdcValue={this.state.EstimatedUsdcValue}
+                    /> {/* render dashboard child component from dashboard.js */}
+                    <br/><br/>
+                </header>
+                {/*
+                     Uses bootstrap cols and rows to make everything all nice and scalable
+                    */}
+                <Container>
+                    <Row>
+                        <Col sm>
+                            <Invest Update={this.Update} waitingOnContract={this.state.waitingOnContract} UpdateWaitingOnContract={this.UpdateWaitingOnContract} /> {/* render invest child component from invest.js */}
+                        </Col>
+                        <Col sm>
+                            <Liquidate Update={this.Update} waitingOnContract={this.state.waitingOnContract} UpdateWaitingOnContract={this.UpdateWaitingOnContract}/> {/* render liquidate child component from liquidate.js */}
+                        </Col>
+                    </Row>
+                </Container>
+                <header className="App-header" style={{
+                    backgroundColor: 'transparent'
+                }}>
+                    {/*
+                     Put all those good legal warning messages up
+                    */}
+                    <h3 style={radiantWhite}>
+                        All initial investments are subject to up to a 1 month holding period to account for slippage.
+                    </h3>
+                    <h3 style={radiantWhite}>
+                        Prices may fluctuate, and Mushroom Capital is not responsible for temporary or permanent loss of
+                        funds.
+                    </h3>
+                </header>
+            </div>
+        );
+    }
 }
 
 export default App;
--- test_interface/src/components/Dashboard.js
+++ test_interface/src/components/Dashboard.js
@@ -3,9 +3,16 @@ import web3 from "../web3"
 import {Card, Form} from "semantic-ui-react";
 import Mushroom from "../Mushroom";
 import Rome from "../Rome";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Invest from "./Invest";
+import Liquidate from "./Liquidate";
+import Container from "react-bootstrap/Container";
 //import Card from 'react-bootstrap/Card'
 //import Box from '@material-ui/core/Box';
 
+//here cause text-shadow css no like
+const moneyBlack = {color: 'black', margin: 0, 'text-shadow': '0px 0px 2px #33cd0c'}
 
 function callBack(err, result) {
     console.warn(result);
@@ -19,63 +26,103 @@ class Dashboard extends Component{
     constructor(props){
         super(props);
         this.state={
-            TotalMush : 0,
-            TotalUsdc: 0,
-            EstimatedUsdcValue: 0,
-            UserMush: 0,
-            UserUsdc: 0
         };
-        this.updateDash = this.updateDash.bind(this);
-        this.updateDash();
     }
 
 
+    /*
+    calls a bunch of the view functions to get data about what cryptos are in the contract and owned by the user and stuff
+    stores these in class state/ component state? which one owns it?
 
-    async updateDash(){
-        var accounts;
-        await web3.eth.getAccounts().then(function(acc){ accounts = acc })
-        
-        let total_mush = await Mushroom.methods.totalSupply().call();
-        let user_mush = await Mushroom.methods.balanceOf(accounts[0]).call();
-
-        let est_value = await Mushroom.methods.viewEstValue().call();
-
-        let total_usdc = await Rome.methods.balanceOf(Mushroom.options.address).call();
-        let user_usdc = await Rome.methods.balanceOf(accounts[0]).call();
-        
-        total_mush = web3.utils.fromWei(total_mush, 'ether');
-        user_mush = web3.utils.fromWei(user_mush, 'ether');
-        est_value = web3.utils.fromWei(est_value, 'ether');
-        total_usdc = web3.utils.fromWei(total_usdc, 'ether');
-        user_usdc = web3.utils.fromWei(user_usdc, 'ether');
+    updates the state, then things down in render actually display the values
+     */
 
 
-        this.setState({TotalMush:total_mush, UserMush: user_mush, TotalUsdc: total_usdc, UserUsdc: user_usdc, EstimatedUsdcValue: est_value });
-    }
-
-/*
-<pre style={{ color: 'white', margin: 0, 'text-shadow': '0px 0px 2px #35FFEC' }}>{'' +
-                'Total MUSH Supply:      ${Total_Mush} MUSH\n' +
-                'Your MUSH Holdings:     ${this.state.UserMush} MUSH'}
- */
+    /*
+    <pre style={{ color: 'white', margin: 0, 'text-shadow': '0px 0px 2px #35FFEC' }}>{'' +
+                    'Total MUSH Supply:      ${Total_Mush} MUSH\n' +
+                    'Your MUSH Holdings:     ${this.state.UserMush} MUSH'}
+     */
 
     render() {
-        var Total_Mush = <pre style={{ color: 'white', margin: 0, 'text-shadow': '0px 0px 2px #35FFEC' }}>Total MUSH Supply                             {this.state.TotalMush} MUSH</pre>
-        var User_Mush = <pre style={{ color: 'white', margin: 0, 'text-shadow': '0px 0px 2px #35FFEC' }}>Your MUSH Holdings                            {this.state.UserMush} MUSH</pre>
-        var Total_Usdc = <pre style={{ color: 'white', margin: 0, 'text-shadow': '0px 0px 2px #35FFEC' }}>Total USDC Holdings of this Contract          {this.state.TotalUsdc} USDC</pre>
-        var User_Usdc = <pre style={{ color: 'white', margin: 0, 'text-shadow': '0px 0px 2px #35FFEC' }}>Your USDC Holdings (outside this project)     {this.state.UserUsdc} USDC</pre>
-        var Est_Value = <pre style={{ color: 'white', margin: 0, 'text-shadow': '0px 0px 2px #35FFEC' }}>Estimated Value of all Mushroom Assets        {this.state.EstimatedUsdcValue} USD </pre>
+        /*
+        Never call something that sets state inside a render funciton. There was gore here.
+        Infinite recursion, bad code BAD CODE. Setting state rerenders which calls render again which
+        calls a thing that sets state again. Dont do it. UpdateStats moved to app class, 1 layer above.
+         */
 
 
         return(
             <div>
-                <h2>
-                    {Total_Mush}
-                    {Total_Usdc}
-                    {Est_Value}
-                    <br/><br/>
-                    {User_Mush}
-                    {User_Usdc}
+                <h2 style={{ color: 'white', margin: 0, 'text-shadow': '0px 0px 2px #33cd0c' }}>
+                    <Container>
+                        <Row>
+                            <Col>
+                                Total MUSH Supply
+                            </Col>
+                            <Col>
+                                <h2 style={moneyBlack}>
+                                    {this.props.TotalMush} MUSH
+                                </h2>
+                            </Col>
+                        </Row>
+                        <br/><br/>
+                        <Row>
+                            <Col>
+                                Total USDC Holdings of this Contract
+                            </Col>
+                            <Col>
+                                <h2 style={moneyBlack}>
+                                    {this.props.TotalUsdc} USDC
+                                </h2>
+                            </Col>
+                        </Row>
+                        <br/><br/>
+                        <Row>
+                            <Col>
+                                Estimated Value of all Mushroom Assets
+                            </Col>
+                            <Col>
+                                <h2 style={moneyBlack}>
+                                    ${this.props.EstimatedUsdcValue} USD
+                                </h2>
+                            </Col>
+                        </Row>
+                        <br/><br/>
+                        <Row>
+                            <Col>
+                                Value of MUSH coin in USDC
+                            </Col>
+                            <Col>
+                                <h2 style={moneyBlack}>
+                                    {this.props.EstimatedUsdcValue/this.props.TotalMush} UDSC
+                                </h2>
+                            </Col>
+                        </Row>
+                        <br/><br/>
+                        <Row>
+                            <Col>
+                                Your MUSH Holdings
+                            </Col>
+                            <Col>
+                                <h2 style={moneyBlack}>
+                                    {this.props.UserMush} MUSH
+                                </h2>
+                            </Col>
+                        </Row>
+                        <br/><br/>
+                        <Row>
+                            <Col>
+                                Your USDC Holdings (outside this project)
+                            </Col>
+                            <Col>
+                                <h2 style={moneyBlack}>
+                                    {this.props.UserUsdc} UDSC
+                                </h2>
+                            </Col>
+                        </Row>
+
+                    </Container>
                 </h2>
             </div>
         )
